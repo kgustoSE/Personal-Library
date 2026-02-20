b782f06442a5f0b85c184c411a14e489837edf29
Kyle Augusto <ks.gusto@gmail.com>
2026-02-20 14:34:15 -0700

    Initial Commit: was practicing, and felt confident so decided to create a repository

---
+++ script.js
@@ -0,0 +1,30 @@
+// Modifiable Variable Array
+let personalLibrary = [];
+
+// Constructor --> Creates the blueprint for what can be placed in our array
+// did some research and the moddern class method automatically places the protypeing correctly
+class Book {
+    constructor(title, author, notes, type, acquired, read, recommended) {
+        this.id = Date.now();
+        this.title = title;
+        this.author = author;
+        this.notes = notes;
+        this.type = type;
+        this.acquired = acquired;
+        this.read = read;
+        this.recommended = recommended;
+    }
+}
+
+
+function saveLibrary() {
+    localStorage.setItem(`my-library`, JSON.stringify(personalLibrary));
+}
+
+function loadLibrary() {
+    const saved = localStorage.getItem(`my-library`);
+    if (!saved) return;
+
+    personalLibrary = JSON.parse(saved)
+
+}
